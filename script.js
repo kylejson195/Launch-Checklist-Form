8f012b3925d5ebf1e7a45021f208bf44b82204b4
Kyle Johnson <kylejohnson@Kyles-Air.fios-router.home>
2020-04-13 14:25:26 -0400

    Sharing is Caring

--- script.js
+++ script.js
@@ -1,7 +1,38 @@
 // Write your JavaScript code here!
 
-/* This block of code shows how to format the HTML once you fetch some planetary JSON!
+
+window.addEventListener("load", function() {
+   let form = document.querySelector("form");
+   form.addEventListener("submit", function(event) {
+      let pilotNameInput = document.querySelector("input[name=pilotName]");
+      let copilotNameInput = document.querySelector("input[name=copilotName]");
+      let fuelLevelInput = document.querySelector("input[name=fuelLevel]");
+      let cargoMassInput = document.querySelector("input[name=cargoMass]");
+       
+      if (pilotNameInput.value === "" || copilotNameInput.value === "") {
+         console.log('First if statement') 
+         alert ("Please enter valid names");
+          event.preventDefault();
+       }
+       if (isNaN(fuelLevelInput.value) === true || isNaN(cargoMassInput.value) === true) {
+         console.log('Second if statement') 
+         alert ("Please enter valid data");
+          event.preventDefault();
+       }
+      });
+   });
+
+
+ /*This block of code shows how to format the HTML once you fetch some planetary JSON!
 <h2>Mission Destination</h2>
+window.addEventListener("load", function() {
+                    fetch("https://handlers.education.launchcode.org/static/planets.json").then( function(response){
+                        response.json().then ( function(json){
+                            const div = document.getElementById("launchStatus")
+                        })
+                    })
+                })
+
 <ol>
    <li>Name: ${}</li>
    <li>Diameter: ${}</li>
